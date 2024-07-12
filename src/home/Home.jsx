a019cac88e2719a4c00bcbe4e42e07bba8895986
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2024-07-13 01:48:41 +0530

    added Product Categories

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './Banner'
+import HomeCate from './HomeCate'
 
 const Home = () => {
   return (
       <div>
       <Banner />
+      <HomeCate />
     </div>
   )
 }
