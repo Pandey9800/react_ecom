7b4e22dae9c870709555e4b42b72ee1d40437281
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2024-07-15 02:22:08 +0530

    created popular post part in the shop page

---
+++ src/shop/Poppost.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const tit = "Most Popular Post";
+
+const postList = [
+    {
+    id:1,
+    imgUrl: '/src/assets/images/blog/10.jpg',
+    imgAlt: 'rajibraj91',
+    title: 'Poor People Campaign Our Resources',
+    date: 'Jun 05,2022',
+    },
+    {
+    id:2,
+    imgUrl: '/src/assets/images/blog/11.jpg',
+    imgAlt: 'rajibraj91',
+    title: 'I mean its 2:22 AM ',
+    date: 'Jun 05,2022',
+    },
+    {
+    id:3,
+    imgUrl: '/src/assets/images/blog/12.jpg',
+    imgAlt: 'rajibraj91',
+    title: 'But we doing it cause we ',
+    date: 'Jun 05,2022',
+    },
+    {
+    id:4,
+    imgUrl: '/src/assets/images/blog/09.jpg',
+    imgAlt: 'rajibraj91',
+    title: 'have hindered our own ass for the least...',
+    date: 'Jun 05,2022',
+    },
+    ]
+
+const Poppost = () => {
+  return (
+      <div className='widget widget-post'>
+          <div className="widget-header">
+              <h5 className='title'>{tit}</h5>
+          </div>
+
+          <ul className="widget-wrapper">
+              {
+                  postList.map((blog, i) => (
+                      <li key={i}> 
+                          <div className="post-thumb">
+                              <Link to={`/blog/${blog.id}`}><img src={blog.imgUrl} alt="" /></Link>
+                        </div>
+                          <div className="post-content">
+                              <Link to={`/blog/${blog.id}`}><h5>{blog.title}</h5></Link>
+                              <p>{blog.date}</p>
+                          </div>
+                    </li>  
+                  ))
+              }
+          </ul>
+    </div>
+  )
+}
+
+export default Poppost
--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -5,6 +5,7 @@ import ProductCards from './ProductCards'
 import Pagination from './Pagination'
 import Search from './Search'
 import ShopCategory from './ShopCategory'
+import Poppost from './Poppost'
 
 const showResults = "Showing 01-12 of 139 Results"
 
@@ -86,6 +87,7 @@ const Shop = () => {
                                   setProducts={setProducts}
                                   selectedCategory={selectedCategory}
                               />
+                              <Poppost/>
                           </aside>
                       </div>
                   </div>
